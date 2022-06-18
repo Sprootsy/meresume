60fc1f2b00bf0844336a37be56be81c035cfb6ed
B++ <f.bernardi89@gmail.com>
2022-06-19 00:30:56 +0200

    feat: first basic layout!

---
+++ web/src/ContactsBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Box, Container, List, Link, ListItem} from '@mui/material';
+
+import './App.css';
+
+export const externalLinks = {
+    "Github": { "url": "https://github.com/JBerny", "icon": "assets/icons/icons8-github.svg" },
+    "Linkedin": { "url": "https://linkedin.com/", "icon": "assets/icons/icons8-linkedin.svg" },
+};
+
+
+export function ContactsBar(props) {
+
+    return (
+        <List className='App-contacts-bar' >
+            {   props.externalLinks ?
+                Object.keys(props.externalLinks).map((key) => {
+                    const extLink = props.externalLinks[key];
+                    return (
+                        <List item key={key} xs={4}>
+                            <ListItem>
+                                    <Link href={extLink['url']} 
+                                        className={"App-logo-link"}
+                                        target={"_blank"}>
+                                        <img src={extLink['icon']} 
+                                            style={{
+                                                height: "48px",
+                                                margin: "4px"
+                                            }} />
+                                        
+                                        {key}
+                                    </Link>
+                            </ListItem>
+                        </List>
+                    );
+                })
+                : null
+            }
+        </List>
+    );
+}
