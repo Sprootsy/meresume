8cae2b5d3d58ff75980c392f84589599e749c622
B++ <f.bernardi89@gmail.com>
2023-08-16 22:29:02 +0200

    feat: added articles

--- web/src/ContactsBar.js
+++ web/src/ContactsBar.js
@@ -1,42 +1,45 @@
+
 import React from 'react';
 import {List, Link, ListItem} from '@mui/material';
 
-import './App.css';
+import './ContactsBar.css';
 
 export const externalLinks = {
     "Github": { "url": "https://github.com/JBerny", "icon": "assets/icons/icons8-github.svg" },
-    "Linkedin": { "url": "https://linkedin.com/", "icon": "assets/icons/icons8-linkedin.svg" },
+    "Linkedin": { "url": "https://linkedin.com/in/francesco-bernardi-ams/", "icon": "assets/icons/icons8-linkedin.svg" },
 };
 
 
 export function ContactsBar(props) {
 
     return (
-        <List className='App-contacts-bar' >
-            {   props.externalLinks ?
-                Object.keys(props.externalLinks).map((key) => {
-                    const extLink = props.externalLinks[key];
-                    return (
-                        <List key={key} xs={4}>
-                            <ListItem>
-                                    <Link href={extLink['url']} 
-                                        className={"App-logo-link"}
-                                        target={"_blank"}>
-                                        <img src={extLink['icon']}
-                                            alt={`Logo for ${key}`}
-                                            style={{
-                                                height: "48px",
-                                                margin: "4px"
-                                            }} />
-                                        
-                                        {key}
-                                    </Link>
-                            </ListItem>
-                        </List>
-                    );
-                })
-                : null
-            }
-        </List>
+        <div>
+            <List className='App-contacts-bar' >
+                {   props.externalLinks ?
+                    Object.keys(props.externalLinks).map((key) => {
+                        const extLink = props.externalLinks[key];
+                        return (
+                            <List key={key} xs={4}>
+                                <ListItem>
+                                        <Link href={extLink['url']} 
+                                            className={"App-logo-link"}
+                                            target={"_blank"}>
+                                            <img src={extLink['icon']}
+                                                alt={`Logo for ${key}`}
+                                                style={{
+                                                    height: "48px",
+                                                    margin: "4px"
+                                                }} />
+                                            
+                                            {key}
+                                        </Link>
+                                </ListItem>
+                            </List>
+                        );
+                    })
+                    : null
+                }
+            </List>
+        </div>
     );
 }
